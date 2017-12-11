5e03ce346814242abfeccac7cb1ed094c88b4a5a
hernan <hernanagf97@gmail.com>
2017-12-11 17:51:05 +0100

    Elimine el tercer syso

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!!!!");
 		System.out.println("Prueba");
-		System.out.println("Prueba 2");
+		
 	}
 
 }
